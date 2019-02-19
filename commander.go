4feb5fceaaf5f17de59348c018a562b84e6bcf99
autom8ter <admin@autom8ter.com>
2019-02-18 17:07:24 -0700

    kube

--- commander.go
+++ commander.go
@@ -2,15 +2,29 @@ package clientz
 
 import (
 	"cloud.google.com/go/firestore"
-	language "cloud.google.com/go/language/apiv1"
+	"cloud.google.com/go/language/apiv1"
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/speech/apiv1"
 	"cloud.google.com/go/storage"
+	"cloud.google.com/go/videointelligence/apiv1"
+	"cloud.google.com/go/vision/apiv1"
 	"firebase.google.com/go"
+	"github.com/bluele/slack"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/huandu/facebook"
+	"github.com/logpacker/PayPal-Go-SDK"
+	"github.com/sfreiberg/gotwilio"
+	stripe "github.com/stripe/stripe-go/client"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/cloudiot/v1"
 	"google.golang.org/api/cloudkms/v1"
+	"k8s.io/client-go/kubernetes"
 	"net/http"
+	talent "google.golang.org/api/jobs/v3"
+	docker "github.com/docker/docker/client"
 )
 
-type Commander struct {
+type Google struct {
 	Project string
 	Bucket  string
 	Handler http.HandlerFunc
@@ -20,4 +34,34 @@ type Commander struct {
 	auth    *cloudkms.Service
 	creds   *google.Credentials
 	lang    *language.Client
+	pub 	*pubsub.Client
+	spch 	*speech.Client
+	vis 	*vision.ImageAnnotatorClient
+	vid 	*videointelligence.Client
+	robots 	*cloudiot.Service
+	tlent 	*talent.Service
 }
+
+type Social struct {
+	fb *facebook.App
+	twit *twitter.Client
+}
+
+type Messenger struct {
+	twil *gotwilio.Twilio
+	slck *slack.Slack
+}
+
+
+type Payment struct {
+	pal *paypalsdk.Client
+	strp *stripe.API
+}
+
+
+type Containers struct {
+	dkr *docker.Client
+	kube *kubernetes.Clientset
+}
+
+
